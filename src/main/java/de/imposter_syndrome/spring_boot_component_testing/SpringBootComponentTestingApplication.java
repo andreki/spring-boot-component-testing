2d998714d45ca27f9557dcfa8bb203ddbd12c5e3
Andreas Kißler <andreas@kissler-online.de>
2022-01-08 15:25:01 +0100

    Initialer Commit

---
+++ src/main/java/de/imposter_syndrome/spring_boot_component_testing/SpringBootComponentTestingApplication.java
@@ -0,0 +1,13 @@
+package de.imposter_syndrome.spring_boot_component_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComponentTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComponentTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/imposter_syndrome/spring_boot_component_testing/SpringBootComponentTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.imposter_syndrome.spring_boot_component_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComponentTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
